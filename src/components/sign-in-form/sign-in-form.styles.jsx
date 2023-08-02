282ee98ea538772281e1e576db4a9b239f531f9b
Bjelogrlic84 <bjelogrlic.ivan.84@gmail.com>
2023-08-02 16:10:33 +0200

    Mobile Sing in

--- src/components/sign-in-form/sign-in-form.styles.jsx
+++ src/components/sign-in-form/sign-in-form.styles.jsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const SignInContainer = styled.div`
   display: flex;
@@ -8,9 +8,20 @@ export const SignInContainer = styled.div`
   h2 {
     margin: 10px 0;
   }
+
+  @media screen and (max-width: 800px) {
+    width: 100%;
+  }
 `;
 
 export const ButtonsContainer = styled.div`
   display: flex;
   justify-content: space-between;
+
+  @media screen and (max-width: 800px) {
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: flex-start;
+    gap: 10px;
+  }
 `;
--- src/routes/authentication/authentication.styles.jsx
+++ src/routes/authentication/authentication.styles.jsx
@@ -1,8 +1,14 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const AuthenticationContainer = styled.div`
   display: flex;
   width: 900px;
   justify-content: space-between;
   margin: 30px auto;
+
+  @media screen and (max-width: 800px) {
+    width: 100%;
+    flex-direction: column;
+    align-items: center;
+  }
 `;
