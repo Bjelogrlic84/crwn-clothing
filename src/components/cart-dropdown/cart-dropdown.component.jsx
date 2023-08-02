27341142c928fd231d0056b96de22d69354680c4
Bjelogrlic84 <bjelogrlic.ivan.84@gmail.com>
2023-08-03 00:22:11 +0200

    Dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,26 +1,47 @@
-import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-
-import { selectCartItems } from '../../store/cart/cart.selector';
-import Button from '../button/button.component';
-import CartItem from '../cart-item/cart-item.component';
+import { useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectCartItems } from "../../store/cart/cart.selector";
+import { useNavigate } from "react-router-dom";
+import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
+import { setIsCartOpen } from "../../store/cart/cart.action";
 
 import {
   CartDropdownContainer,
   EmptyMessage,
   CartItems,
-} from './cart-dropdown.styles';
+} from "./cart-dropdown.styles";
 
 const CartDropdown = () => {
   const cartItems = useSelector(selectCartItems);
   const navigate = useNavigate();
+  const cartRef = useRef();
+  const dispatch = useDispatch();
+
+  const [isCheckoutClicked, setIsCheckoutClicked] = useState(false);
 
   const goToCheckoutHandler = () => {
-    navigate('/checkout');
+    setIsCheckoutClicked(true);
+    navigate("/checkout");
+    dispatch(setIsCartOpen(false));
   };
 
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (!cartRef.current.contains(event.target) && !isCheckoutClicked) {
+        dispatch(setIsCartOpen(false));
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [dispatch, isCheckoutClicked]);
+
   return (
-    <CartDropdownContainer>
+    <CartDropdownContainer ref={cartRef}>
       <CartItems>
         {cartItems.length ? (
           cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
